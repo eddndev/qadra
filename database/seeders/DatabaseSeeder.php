6ded37e4335fd38220ad4eddfd73db9e0ddf0c68
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-12-03 14:18:56 -0600

    feat: Add DatabaseSeeder to orchestrate initial database seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SubscriptionTiersSeeder::class,
             PermissionsAndRolesSeeder::class,
+            CrimeTypesSeeder::class,
+            PrecautionaryMeasureTypesSeeder::class,
         ]);
         
         // User::factory(10)->create();
