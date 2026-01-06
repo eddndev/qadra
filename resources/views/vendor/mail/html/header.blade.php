fd3d56109c62d685711250c0bd5d441552d19646
eddndev <contacto@eddndev.com>
2026-01-05 23:51:30 -0600

    style(mail): use png logo for broader client compatibility

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('logo.svg') }}" class="logo" alt="Qadra Logo">
+            <img src="{{ asset('logo.png') }}" class="logo" alt="Qadra Logo">
         </a>
     </td>
 </tr>
